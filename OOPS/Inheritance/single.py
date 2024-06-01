410cb4fb8f4efd406d8420c8195686036ffc42e8
Yash Kunwar <166682048+Yash-Kunwar@users.noreply.github.com>
2024-06-01 07:26:29 +0530

    Single Inheritance

---
+++ OOPS/Inheritance/single.py
@@ -0,0 +1,23 @@
+class Employee:
+    company = 'google'
+
+    def showDetails(self):
+        print('this is employee class')
+
+
+class Programer(Employee):
+    language = 'python'
+    company = 'YouTube'
+
+    def getLang(self):
+        print(f'the language is {self.language}')
+
+    def showDetails(self):
+        print('this is programer class')
+
+
+e = Employee()
+p = Programer()
+e.showDetails()
+p.showDetails()
+print(p.company)
