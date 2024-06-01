dce0fb538e2e7f5aec0e285c98493c8364ef6ba2
Yash Kunwar <166682048+Yash-Kunwar@users.noreply.github.com>
2024-06-01 07:04:57 +0530

    Conditional  Expressions i.e. IF, ELSE and ELIF

---
+++ Functional Programming/if-else-elif.py
@@ -0,0 +1,44 @@
+a = 50
+
+# if-else-elif ladder
+
+if (a < 3):
+    print("a>3")
+elif (a > 7):
+    print("a>7")
+else:
+    print("a!>3,7")
+
+# multiple if statements
+if (a < 3):
+    print("a<3")
+if (a > 13):
+    print("a>13")
+if (a > 7):
+    print("a>7")
+
+print("Donaey")
+
+age = int(input("enter ur age: "))
+if age >= 18:
+    print("yes")
+else:
+    print("no")
+
+# logical and relational operators
+age = 59
+if (age > 34 or age < 56):
+    print('yes')
+else:
+    print('no')
+
+# in and is
+a = None
+if a is None:
+    print('yes')
+else:
+    print('no')
+print(a)
+
+a = [45, 56, 6]
+print(40 in a)
