4c94fe171e03a5c145ef57175cda64c98b14dc59
Yash Kunwar <166682048+Yash-Kunwar@users.noreply.github.com>
2024-06-01 06:57:51 +0530

    Using LISTS and its Operations

---
+++ Functional Programming/lists.py
@@ -0,0 +1,10 @@
+l1 = [1, 8, 7, 2, 21, 15]
+print(l1)
+l1.sort()
+l1.reverse()
+l1.append(45)  # **add element @ the end of list
+l1.append(63)
+l1.insert(1, 88)  # insert element @ specific index
+l1.pop(2)  # deletes index
+l1.remove(21)  # removes element
+print(l1)
