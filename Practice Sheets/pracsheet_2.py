b90d116f3b894444353e37d4d9e70edf41a26ef7
Yash Kunwar <166682048+Yash-Kunwar@users.noreply.github.com>
2024-06-01 15:34:11 +0530

    Practice problems for Variables and Data Types

---
+++ Practice Sheets/pracsheet_2.py
@@ -0,0 +1,47 @@
+# add 2 nos
+a = input("enter 1st no. ")
+a = int(a)
+b = input("enter 2nd no. ")
+b = int(b)
+print(a+b)
+
+print('****************************')
+
+# n/2 remainder
+num1 = input("enter a no. ")
+num1 = int(num1)
+print(num1 % 2)
+
+print('****************************')
+
+# Check type of variable
+check = input("enter: ")
+print(type(check))
+
+print('****************************')
+
+# compare 2 variables
+x = input("enter no. 1: ")
+x = int(x)
+y = input("enter no. 2: ")
+y = int(y)
+if (x > y):
+    print(x, ">", y)
+else:
+    print(x, "<", y)
+
+print('****************************')
+
+# avg. of 2 no.s
+a1 = input("enter no. 1: ")
+a1 = int(a1)
+b1 = input("enter no. 2: ")
+b1 = int(b1)
+print((a1+b1)/2)
+
+print('****************************')
+
+# sqaure of a no.
+sq = input("enter a no.: ")
+sq = int(sq)
+print(sq*sq)
