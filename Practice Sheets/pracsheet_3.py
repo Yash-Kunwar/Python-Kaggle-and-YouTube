709383f8af63b9405d93e6bf084381eef3032f7d
Yash Kunwar <166682048+Yash-Kunwar@users.noreply.github.com>
2024-06-01 15:38:44 +0530

    Practice problems for Strings

---
+++ Practice Sheets/pracsheet_3.py
@@ -0,0 +1,22 @@
+# good afternoon "name"
+name = input("enter name ")
+print("good afternoon", name, "!!")
+
+print('****************************')
+
+# meri selection ho gyi hai!.?
+date = input("enter date ")
+print("dear", name, "\n you are selected!", "\n", date)
+
+print('****************************')
+
+# detect double spaces
+text = input("enter text ")
+print(text.count("  "), "double spaces found")
+print(text.replace("  ", " "))
+
+print('****************************')
+
+# # apply escape sequence
+letter = "dear yash,\nthis python course is osum,\nthanks!"
+print(letter)
